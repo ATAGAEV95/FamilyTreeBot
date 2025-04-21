c2443a1670facae640d55a112e601b44860acdf5
ATAGAEV <preator-man@mail.ru>
2025-04-21 13:07:24 +0300

    2025-04-21

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-TG_TOKEN = 'Ваш токен телеграмм бота'
-
-# Ваше подключение к БД Postgres
-DATABASE_URL = "postgresql+asyncpg://login:password@192.168.0.1:5432/bdname"
-
-# Ваша схема из БД Postgres, по умолчанию "public"
+TG_TOKEN = '8003815284:AAGxhfv8uEoZbUnCkDDdp5GhkpgoRWM2178'
+DATABASE_URL = "postgresql+asyncpg://postgres:Superflash95@185.236.24.242:5432/family"
 SCHEMA = "public"
