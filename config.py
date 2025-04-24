52d386b3fd3b05bff504f0a0bf48a44ba0dc1a20
ATAGAEV <preator-man@mail.ru>
2025-04-24 18:25:53 +0300

    2025-04-24

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-TG_TOKEN = 'Ваш токен телеграмм бота'
+# TG_TOKEN = 'Ваш токен телеграмм бота'
 
 # Ваше подключение к БД Postgres
-DATABASE_URL = "postgresql+asyncpg://login:password@192.168.0.1:5432/bdname"
+# DATABASE_URL = "postgresql+asyncpg://login:password@192.168.0.1:5432/bdname"
 
 # Ваша схема из БД Postgres, по умолчанию "public"
+# SCHEMA = "public"
+
+import os
+
+TG_TOKEN = os.getenv("TG_TOKEN")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 SCHEMA = "public"
